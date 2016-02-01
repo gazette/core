2ce1229da098f3853c81bd9695547eefb68e55ee
Johnny Graettinger <johnny.graettinger@liveramp.com>
2016-01-31 21:04:42 -0500

    Introduce `consensus` package with low-level Etcd operations
    `consensus` is a rough replacement for the `discovery` package, which:
    
    * Re-pins on the official Etcd 2 golang client.
    
    * Removes EtcdMemoryService. Going forward, unit tests will run against
     "real" Etcd, and will require that an Etcd endpoint be available.
      See also kubernetes/kubernets#15349 (similar story and outcome).
    
    * Introduces low-level helpers and operations for:
      - Building more reliable Watchers (RetryWatcher).
      - Patching a tree with an etcd.Responses (ported from `discovery`).
      - Retrieving children (Child()).
      - Performing zipped iteration of actual and expected nodes
        (ForEachChild()).
      - Deep-copying etcd.Nodes (CopyNode() / CopyNodes()).
    
    * Removes EtcdService abstractions. Official etcd interfaces (etcd.KeysAPI)
      are now preferred. The former `discovery` Subscribe() API is eliminated,
      as it results in un-avoidable data races. Instead, callers must compose a
      RetryWatcher with PatchTree() to achieve similar functionality.
    
    Issue #307

---
+++ tree_ops.go
@@ -0,0 +1,227 @@
+package consensus
+
+import (
+	"errors"
+	"sort"
+	"strings"
+
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	etcd "github.com/coreos/etcd/client"
+	"github.com/coreos/etcd/store"
+)
+
+var (
+	EtcdUpsertOps = map[string]struct{}{
+		store.CompareAndSwap: {},
+		store.Create:         {},
+		store.Get:            {},
+		store.Set:            {},
+		store.Update:         {},
+	}
+	EtcdRemoveOps = map[string]struct{}{
+		store.CompareAndDelete: {},
+		store.Delete:           {},
+		store.Expire:           {},
+	}
+)
+
+// Returns whether an etcd.Response.Action is an insert or update.
+func isEtcdUpsertOp(action string) bool {
+	_, ok := EtcdUpsertOps[action]
+	return ok
+}
+
+// Returns whether an etcd.Response.Action is a removal.
+func isEtcdRemoveOp(action string) bool {
+	_, ok := EtcdRemoveOps[action]
+	return ok
+}
+
+// Precondition: |parent.Nodes| is sorted. Returns the immediate child of
+// |parent| having name |name|, or nil if no such child exists.
+func Child(parent *etcd.Node, name string) *etcd.Node {
+	prefix := len(parent.Key) + 1
+
+	ind := sort.Search(len(parent.Nodes), func(i int) bool {
+		return parent.Nodes[i].Key[prefix:] >= name
+	})
+	if ind < len(parent.Nodes) && parent.Nodes[ind].Key[prefix:] == name {
+		return parent.Nodes[ind]
+	}
+	return nil
+}
+
+// Precondition: |names| is sorted, as are |parent.Nodes|. For |names| and
+// |parent.Nodes|, invokes |cb| with the outer join of a name and its etcd.Node.
+func forEachChild(parent *etcd.Node, names []string, cb func(string, *etcd.Node)) {
+	nodes := parent.Nodes
+	for len(nodes) != 0 || len(names) != 0 {
+		// Integrity assertions.
+		if len(names) > 1 && names[0] >= names[1] {
+			panic("names not ordered")
+		}
+		if len(nodes) > 1 && nodes[0].Key >= nodes[1].Key {
+			panic("nodes not ordered")
+		}
+		if len(names) != 0 && strings.IndexByte(names[0], '/') != -1 {
+			panic("names cannot include slashes")
+		}
+
+		var nodeName string
+		if len(nodes) != 0 {
+			nodeName = nodes[0].Key[len(parent.Key)+1:]
+		}
+
+		if len(nodes) == 0 || (len(names) != 0 && nodeName > names[0]) {
+			// Current name is not a child of |parent|.
+			cb(names[0], &etcd.Node{Key: parent.Key + "/" + names[0]})
+			names = names[1:]
+		} else if len(names) == 0 || nodeName < names[0] {
+			// Current node is not in |names|.
+			cb(nodeName, nodes[0])
+			nodes = nodes[1:]
+		} else {
+			// Current name and node match.
+			cb(nodeName, nodes[0])
+			nodes = nodes[1:]
+			names = names[1:]
+		}
+	}
+}
+
+// Precondition: |node| is recursively sorted. Performs a recursive search
+// rooted at |node| to identify the |parent| of |key|, and the |index| where
+// |key| exists or would be inserted. If a required parent of |key| does not
+// exist, its insertion-point is returned.
+func FindNode(node *etcd.Node, key string) (parent *etcd.Node, index int) {
+	if len(key) <= len(node.Key) {
+		return nil, -1
+	}
+	for {
+		var ind int
+		var intermediate string
+
+		if slash := strings.IndexByte(key[len(node.Key)+1:], '/'); slash != -1 {
+			// Search for the next intermediate directory node.
+			intermediate = key[:len(node.Key)+1+slash]
+			ind = sort.Search(len(node.Nodes), func(i int) bool {
+				return node.Nodes[i].Key >= intermediate
+			})
+		} else {
+			// No directories remain. Look for the exact key.
+			ind = sort.Search(len(node.Nodes), func(i int) bool {
+				return node.Nodes[i].Key >= key
+			})
+		}
+
+		if ind < len(node.Nodes) && intermediate == node.Nodes[ind].Key {
+			// Node is a parent. Iterate.
+			node = node.Nodes[ind]
+		} else {
+			// |ind| is the node, or is it's insertion point.
+			return node, ind
+		}
+	}
+}
+
+// Precondition: |tree| and |response| are recursively sorted. Patches |tree|
+// to reflect |response|, returning the new tree root (which may be |tree|).
+// PatchTree() can be fed responses of a RetryWatcher() to provide an
+// incrementally-built consistent view of a keyspace.
+func PatchTree(tree *etcd.Node, response *etcd.Response) (*etcd.Node, error) {
+	if response.Action == store.Get && (tree == nil || tree.Key == response.Node.Key) {
+		// Init or replace the entire tree.
+		return response.Node, nil
+	} else if tree == nil {
+		return nil, errors.New("not a 'get', and tree is nil")
+	} else if !strings.HasPrefix(response.Node.Key, tree.Key) {
+		return tree, errors.New("expected response to be a tree subnode")
+	}
+
+	parent, ind := FindNode(tree, response.Node.Key)
+
+	// Did we find the exact target node?
+	if ind < len(parent.Nodes) && parent.Nodes[ind].Key == response.Node.Key {
+		// Indicates an inconsistency.
+		if response.Action == store.Create {
+			return tree, errors.New("unexpected create")
+		}
+
+		if isEtcdUpsertOp(response.Action) {
+			parent.Nodes[ind] = response.Node
+		} else {
+			// Remove the target node.
+			if ind+1 < len(parent.Nodes) {
+				copy(parent.Nodes[ind:], parent.Nodes[ind+1:])
+			}
+			parent.Nodes = parent.Nodes[:len(parent.Nodes)-1]
+		}
+		return tree, nil
+	}
+
+	// The target node doesn't exist. A removal op indicates an inconsistency.
+	if isEtcdRemoveOp(response.Action) {
+		return tree, errors.New("unexpected removal")
+	}
+
+	spliceNode := response.Node
+
+	// Build required intermediate directories of the key.
+	for subkey := response.Node.Key[len(parent.Key)+1:]; true; {
+		if i := strings.LastIndexByte(subkey, '/'); i == -1 {
+			break // No intermediate components remain.
+		} else {
+			subkey = subkey[:i]
+		}
+
+		spliceNode = &etcd.Node{
+			Key:           parent.Key + "/" + subkey,
+			Dir:           true,
+			Nodes:         etcd.Nodes{spliceNode},
+			CreatedIndex:  spliceNode.CreatedIndex,
+			ModifiedIndex: spliceNode.ModifiedIndex,
+		}
+	}
+
+	// Insert |spliceNode| into |parent| at |ind|.
+	parent.Nodes = append(parent.Nodes, spliceNode)
+	if ind+1 < len(parent.Nodes) {
+		copy(parent.Nodes[ind+1:], parent.Nodes[ind:])
+		parent.Nodes[ind] = spliceNode
+	}
+
+	return tree, nil
+}
+
+// Creates a deep-copy of |node|.
+func CopyNode(node *etcd.Node) *etcd.Node {
+	result := *node
+	result.Nodes = CopyNodes(result.Nodes)
+	return &result
+}
+
+// Creates a deep-copy of |nodes|.
+func CopyNodes(nodes etcd.Nodes) etcd.Nodes {
+	if nodes == nil {
+		return nil
+	}
+	result := make(etcd.Nodes, len(nodes))
+	for i := range nodes {
+		result[i] = CopyNode(nodes[i])
+	}
+	return result
+}
+
+// Simple typedef of KeysAPI, presented here for mock generation.
+type KeysAPI interface {
+	// TODO(johnny): We'd prefer to compose etcd.KeysAPI. However,
+	// we rely on mockery for mock generation, and first require a fix for:
+	// https://github.com/vektra/mockery/issues/18
+	Get(ctx context.Context, key string, opts *etcd.GetOptions) (*etcd.Response, error)
+	Set(ctx context.Context, key, value string, opts *etcd.SetOptions) (*etcd.Response, error)
+	Delete(ctx context.Context, key string, opts *etcd.DeleteOptions) (*etcd.Response, error)
+	Create(ctx context.Context, key, value string) (*etcd.Response, error)
+	CreateInOrder(ctx context.Context, dir, value string, opts *etcd.CreateInOrderOptions) (*etcd.Response, error)
+	Update(ctx context.Context, key, value string) (*etcd.Response, error)
+	Watcher(key string, opts *etcd.WatcherOptions) etcd.Watcher
+}
---
+++ tree_ops_test.go
@@ -0,0 +1,280 @@
+package consensus
+
+import (
+	"testing"
+
+	etcd "github.com/coreos/etcd/client"
+	"github.com/coreos/etcd/store"
+	gc "github.com/go-check/check"
+)
+
+type TreeOpsSuite struct{}
+
+func (s *TreeOpsSuite) TestChildLookup(c *gc.C) {
+	fixture := s.fixture()
+
+	c.Check(Child(fixture, "aa"), gc.Equals, (*etcd.Node)(nil))
+	c.Check(Child(fixture, "aaa"), gc.Equals, fixture.Nodes[0])
+	c.Check(Child(fixture, "aaaa"), gc.Equals, (*etcd.Node)(nil))
+	c.Check(Child(fixture, "bbb"), gc.Equals, fixture.Nodes[1])
+	c.Check(Child(fixture, "bbbb"), gc.Equals, (*etcd.Node)(nil))
+	c.Check(Child(fixture, "ccc"), gc.Equals, fixture.Nodes[2])
+	c.Check(Child(fixture, "ddd"), gc.Equals, (*etcd.Node)(nil))
+}
+
+func (s *TreeOpsSuite) TestForEachChildIteration(c *gc.C) {
+	fixture := s.fixture()
+
+	var expect map[string]*etcd.Node
+	cb := func(name string, node *etcd.Node) {
+		c.Check(node.Key, gc.DeepEquals, expect[name].Key)
+		c.Check(node.Dir, gc.Equals, expect[name].Dir)
+		delete(expect, name)
+	}
+
+	expect = map[string]*etcd.Node{
+		"aaa": {Key: "/foo/aaa", Dir: true}, // Node before beginning of names.
+		"bbb": {Key: "/foo/bbb", Dir: true}, // Node & name match.
+		"cc":  {Key: "/foo/cc"},             // Name inserted in middle.
+		"ccc": {Key: "/foo/ccc", Dir: true}, // Node after end of names.
+	}
+	forEachChild(fixture, []string{"bbb", "cc"}, cb)
+	c.Check(expect, gc.DeepEquals, map[string]*etcd.Node{})
+
+	expect = map[string]*etcd.Node{
+		"aa":   {Key: "/foo/aa"},             // Name before beginning of Nodes.
+		"aaa":  {Key: "/foo/aaa", Dir: true}, // Node & name match.
+		"bbb":  {Key: "/foo/bbb", Dir: true}, // Node inserted in middle.
+		"ccc":  {Key: "/foo/ccc", Dir: true}, // Node & name match.
+		"dddd": {Key: "/foo/dddd"},           // Name after end of Nodes.
+	}
+	forEachChild(fixture, []string{"aa", "aaa", "ccc", "dddd"}, cb)
+	c.Check(expect, gc.DeepEquals, map[string]*etcd.Node{})
+
+	c.Check(func() {
+		forEachChild(fixture, []string{"wrong", "order"}, cb)
+	}, gc.PanicMatches, "names not ordered")
+
+	c.Check(func() {
+		forEachChild(fixture, []string{"bad/slash"}, cb)
+	}, gc.PanicMatches, "names cannot include slashes")
+
+	fixture.Nodes[0], fixture.Nodes[1] = fixture.Nodes[1], fixture.Nodes[0]
+	c.Check(func() {
+		forEachChild(fixture, []string{}, cb)
+	}, gc.PanicMatches, "nodes not ordered")
+}
+
+func (s *TreeOpsSuite) TestFindNode(c *gc.C) {
+	fixture := s.fixture()
+
+	parent, ind := FindNode(fixture, "/foo")
+	c.Check(parent, gc.IsNil)
+	c.Check(ind, gc.Equals, -1)
+
+	parent, ind = FindNode(fixture, "/foo/aaa/0")
+	c.Check(parent, gc.Equals, fixture.Nodes[0])
+	c.Check(ind, gc.Equals, 0)
+	parent, ind = FindNode(fixture, "/foo/aaa/3")
+	c.Check(parent, gc.Equals, fixture.Nodes[0])
+	c.Check(ind, gc.Equals, 1)
+	parent, ind = FindNode(fixture, "/foo/aaa/4")
+	c.Check(parent, gc.Equals, fixture.Nodes[0])
+	c.Check(ind, gc.Equals, 2)
+	parent, ind = FindNode(fixture, "/foo/bb")
+	c.Check(parent, gc.Equals, fixture)
+	c.Check(ind, gc.Equals, 1)
+	parent, ind = FindNode(fixture, "/foo/bbb")
+	c.Check(parent, gc.Equals, fixture)
+	c.Check(ind, gc.Equals, 1)
+	parent, ind = FindNode(fixture, "/foo/bbbb")
+	c.Check(parent, gc.Equals, fixture)
+	c.Check(ind, gc.Equals, 2)
+	parent, ind = FindNode(fixture, "/foo/bbb/ddd/eee/1")
+	c.Check(parent, gc.Equals, fixture.Nodes[1])
+	c.Check(ind, gc.Equals, 2)
+	parent, ind = FindNode(fixture, "/foo/ccc/apple/one")
+	c.Check(parent, gc.Equals, fixture.Nodes[2].Nodes[0])
+	c.Check(ind, gc.Equals, 0)
+	parent, ind = FindNode(fixture, "/foo/ccc/apple/two")
+	c.Check(parent, gc.Equals, fixture.Nodes[2].Nodes[0])
+	c.Check(ind, gc.Equals, 1)
+	parent, ind = FindNode(fixture, "/foo/ccc/banana/two")
+	c.Check(parent, gc.Equals, fixture.Nodes[2].Nodes[1])
+	c.Check(ind, gc.Equals, 0)
+}
+
+func (s *TreeOpsSuite) TestPatchTree(c *gc.C) {
+	var err error
+	{
+		var tree *etcd.Node
+		fixture := s.fixture()
+
+		tree, err = PatchTree(tree, &etcd.Response{Node: fixture, Action: store.Get})
+		c.Check(err, gc.IsNil)
+		c.Check(tree, gc.Equals, fixture)
+
+		// Refresh of tree content.
+		tree, err = PatchTree(tree, &etcd.Response{Node: s.fixture(), Action: store.Get})
+		c.Check(err, gc.IsNil)
+
+		// Tree was swapped out with new fixture instance.
+		c.Check(tree, gc.Not(gc.Equals), fixture)
+		c.Check(tree, gc.DeepEquals, fixture)
+
+		// An attempt to patch a sibling tree branch fails.
+		tree, err = PatchTree(tree, &etcd.Response{Node: &etcd.Node{Key: "/bar"}, Action: store.Get})
+		c.Check(err, gc.ErrorMatches, "expected response to be a tree subnode")
+	}
+	{
+		// Update of a present, nested value.
+		tree := s.fixture()
+
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node:   &etcd.Node{Key: "/foo/bbb/c", Value: "patched update"},
+			Action: store.Set,
+		})
+		c.Check(err, gc.IsNil)
+		c.Check(tree.Nodes[1].Nodes[1].Value, gc.Equals, "patched update")
+
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node:   &etcd.Node{Key: "/foo/bbb/c", Value: "patched update"},
+			Action: store.Create,
+		})
+		c.Check(err, gc.ErrorMatches, "unexpected create")
+		c.Check(tree, gc.Not(gc.IsNil))
+	}
+	{
+		// Deletion of a nested value.
+		tree := s.fixture()
+		c.Check(tree.Nodes[0].Nodes, gc.HasLen, 2) // Precondition.
+
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/aaa/3", Value: ""}, Action: store.Expire})
+		c.Check(err, gc.IsNil)
+
+		c.Check(tree.Nodes[0].Nodes, gc.HasLen, 1)
+		c.Check(tree.Nodes[0].Nodes[0].Key, gc.Equals, "/foo/aaa/1")
+
+		_, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/aaa/3", Value: ""}, Action: store.Expire})
+		c.Check(err, gc.ErrorMatches, "unexpected removal")
+		c.Check(tree, gc.Not(gc.IsNil))
+
+		c.Check(tree.Nodes[1].Nodes, gc.HasLen, 2) // Precondition.
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/bbb/a", Value: ""}, Action: store.CompareAndDelete})
+		c.Check(err, gc.IsNil)
+
+		c.Check(tree.Nodes[1].Nodes, gc.HasLen, 1)
+		c.Check(tree.Nodes[1].Nodes[0].Key, gc.Equals, "/foo/bbb/c")
+	}
+	{
+		// Insertions of new values.
+		tree := s.fixture()
+		c.Check(tree.Nodes[0].Nodes, gc.HasLen, 2) // Precondition.
+
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/aaa/0", Value: "zero"}, Action: store.Set})
+		c.Check(err, gc.IsNil)
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/aaa/2", Value: "two"}, Action: store.Set})
+		c.Check(err, gc.IsNil)
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node: &etcd.Node{Key: "/foo/aaa/4", Value: "four"}, Action: store.Set})
+		c.Check(err, gc.IsNil)
+
+		c.Check(tree.Nodes[0].Nodes, gc.HasLen, 5)
+		c.Check(tree.Nodes[0].Nodes[0].Key, gc.Equals, "/foo/aaa/0")
+		c.Check(tree.Nodes[0].Nodes[1].Key, gc.Equals, "/foo/aaa/1")
+		c.Check(tree.Nodes[0].Nodes[2].Key, gc.Equals, "/foo/aaa/2")
+		c.Check(tree.Nodes[0].Nodes[3].Key, gc.Equals, "/foo/aaa/3")
+		c.Check(tree.Nodes[0].Nodes[4].Key, gc.Equals, "/foo/aaa/4")
+	}
+	{
+		// Insertion requiring intermediate directories.
+		tree := s.fixture()
+
+		apple := tree.Nodes[2].Nodes[0]
+		c.Check(apple.Nodes, gc.HasLen, 1) // Precondition.
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node:   &etcd.Node{Key: "/foo/ccc/apple/on/two/three", Value: "insert"},
+			Action: store.Set,
+		})
+		c.Check(err, gc.IsNil)
+
+		c.Check(apple.Nodes, gc.HasLen, 2)
+
+		c.Check(apple.Nodes[0].Key, gc.Equals, "/foo/ccc/apple/on")
+		c.Check(apple.Nodes[0].Dir, gc.Equals, true)
+		c.Check(apple.Nodes[1].Key, gc.Equals, "/foo/ccc/apple/one")
+
+		appleOn := apple.Nodes[0]
+		c.Check(appleOn.Nodes[0].Key, gc.Equals, "/foo/ccc/apple/on/two")
+		c.Check(appleOn.Nodes[0].Dir, gc.Equals, true)
+
+		appleOnTwo := appleOn.Nodes[0]
+		c.Check(appleOnTwo.Nodes[0].Key, gc.Equals, "/foo/ccc/apple/on/two/three")
+		c.Check(appleOnTwo.Nodes[0].Dir, gc.Equals, false)
+		c.Check(appleOnTwo.Nodes[0].Value, gc.Equals, "insert")
+	}
+	{
+		// Deletion of a directory.
+		tree := s.fixture()
+		c.Check(tree.Nodes, gc.HasLen, 3) // Precondition.
+
+		tree, err = PatchTree(tree, &etcd.Response{
+			Node:   &etcd.Node{Key: "/foo/aaa", Value: ""},
+			Action: store.Delete,
+		})
+		c.Check(err, gc.IsNil)
+
+		c.Check(tree.Nodes, gc.HasLen, 2)
+		c.Check(tree.Nodes[0].Key, gc.Equals, "/foo/bbb")
+		c.Check(tree.Nodes[1].Key, gc.Equals, "/foo/ccc")
+	}
+}
+
+func (s *TreeOpsSuite) TestDeepCopy(c *gc.C) {
+	tree1 := s.fixture()
+	tree2 := CopyNode(tree1)
+
+	// Expect trees are deeply equal, but that instances are recursively distinct.
+	c.Check(tree1, gc.DeepEquals, tree2)
+	c.Check(tree1, gc.Not(gc.Equals), tree2)
+	c.Check(tree1.Nodes[0], gc.Not(gc.Equals), tree2.Nodes[0])
+	c.Check(tree1.Nodes[0].Nodes[0], gc.Not(gc.Equals), tree2.Nodes[0].Nodes[0])
+}
+
+func (s *TreeOpsSuite) fixture() *etcd.Node {
+	return &etcd.Node{
+		Key: "/foo",
+		Nodes: etcd.Nodes{
+			{
+				Key:   "/foo/aaa",
+				Dir:   true,
+				Nodes: etcd.Nodes{{Key: "/foo/aaa/1"}, {Key: "/foo/aaa/3"}},
+			},
+			{
+				Key:   "/foo/bbb",
+				Dir:   true,
+				Nodes: etcd.Nodes{{Key: "/foo/bbb/a"}, {Key: "/foo/bbb/c"}},
+			},
+			{
+				Key: "/foo/ccc",
+				Dir: true,
+				Nodes: etcd.Nodes{
+					{
+						Key:   "/foo/ccc/apple",
+						Dir:   true,
+						Nodes: etcd.Nodes{{Key: "/foo/ccc/apple/one"}},
+					},
+					{Key: "/foo/ccc/banana"}},
+			},
+		},
+	}
+}
+
+var _ = gc.Suite(&TreeOpsSuite{})
+
+func Test(t *testing.T) { gc.TestingT(t) }
