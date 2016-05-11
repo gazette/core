0b81f99fd363c576a7425d069f203ad1c8cee0c3
Joshua Kwan <joshua.kwan@liveramp.com>
2016-05-11 14:17:41 -0400

    Make some keepalive helpers, and use them.
    - keepalive.Dialer is a net.Dialer with KeepAlive enabled.
      Use it in gazette's replicate_client.go.
    
    - keepalive.TCPListener wraps a net.TCPListener with keepalive
      characteristics. Use it in places where we need to call net.Listen
      manually, namely the Gazette listening socket.
    
    Issue #1717

---
+++ keepalive.go
@@ -0,0 +1,34 @@
+package keepalive
+
+import (
+	"net"
+	"time"
+)
+
+// NOTE(joshk): Copied from the invocation in http.DefaultTransport
+var Dialer = &net.Dialer{
+	Timeout:   30 * time.Second,
+	KeepAlive: 30 * time.Second,
+}
+
+// NOTE(joshk): Copied, renamed and exported from:
+// https://golang.org/src/net/http/server.go?s=65528:65583#L2418
+// so we can use net.Listen + http.Serve.
+
+// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+type TCPListener struct {
+	*net.TCPListener
+}
+
+func (ln TCPListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
+}
