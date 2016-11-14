59e6a4f1fe2584a31199277a20ea275417c695d3
Johnny Graettinger <johnny.graettinger@liveramp.com>
2016-11-14 17:01:56 -0500

    vaulted IDs, published message validation, and pruning
    Add `Validate` method to PixelEvent which is invoked by SimplePublisher.
    Validate changes that the event is well formed, and (in the future) that
    it doesn't include sensitive identifiers we don't wish to log.
    
    Add "pointer" IDs which stand in for sensitive IDs in the graph, and
    extend GraphNode with an optional dereferenced `RawId`.
    
    Add pruning implementions for Links and Segments, which clear out
    tombstone records.
    
    Fix Id.SimpleString, which had incorrectly included a namespace in all
    cases.
    
    Issue #2163

--- message/simple_publisher.go
+++ message/simple_publisher.go
@@ -3,20 +3,30 @@ package message
 import (
 	"sync"
 
+	log "github.com/Sirupsen/logrus"
+
 	"github.com/pippio/gazette/journal"
 	"github.com/pippio/gazette/topic"
 )
 
-// Implementation of topic.Publisher, which frames, routes, and writes
-// the message to the appropriate Journal.
+// Implementation of topic.Publisher which optionally validates, frames,
+// routes, and writes messages to the appropriate Journal.
 type SimplePublisher struct {
 	journal.Writer
 }
 
 func (p SimplePublisher) Publish(msg topic.Marshallable, to *topic.Description) error {
-	buffer := publishBufferPool.Get().([]byte)
+	if v, ok := msg.(interface {
+		Validate() error
+	}); ok {
+		if err := v.Validate(); err != nil {
+			// TODO(johnny): Temporarily log. In the future, return error.
+			log.WithField("err", err).Error("event validation failed")
+		}
+	}
+	var buffer = publishBufferPool.Get().([]byte)
 
-	err := Frame(msg, &buffer)
+	var err = Frame(msg, &buffer)
 	if err == nil {
 		_, err = p.Writer.Write(to.RoutedJournal(msg), buffer)
 	}
