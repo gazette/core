1f0f9fb5e54ce4d427febebf37b8cfbeb91c8bd5
Joshua Kwan <joshua.kwan@liveramp.com>
2017-03-22 11:00:51 -0400

    gazdu, gazconsumer updates
    gazconsumer
    ===========
    
    - remove concept of 'infinity lag' and instead specify lag as greater
      than or equal to the sum of known lags
    - check "ready" or "recovering" state and make that part of verbose
      output
    - convert short mode to monitor mode and support specifying prefixes
      that are not necessarily consumers themselves, but are directories
      that contain consumers. This brings gazconsumer to parity with
      gazstatsd and allows us to run it standalone.
    
    gazdu
    =====
    
    - add go-humanize capability (-mode=human or shortcut -h)

--- tool/gazconsumer/main.go
+++ tool/gazconsumer/main.go
@@ -34,11 +34,17 @@ const (
 	bytesPerGigabyte    = 1024 * 1024 * 1024
 	journalNotBeingRead = -1
 	minimumLag          = 1048576 * 512
-	infinityLag         = -1
 )
 
 var (
-	short = flag.Bool("short", false, "Provide lag estimate in GiB only.")
+	monitor         = flag.Bool("monitor", false, "Use arguments to provide metrics to Prometheus.")
+	monitorInterval = flag.Duration("monitorInterval", time.Minute, "How often to update metrics.")
+	prefixList      prefixFlagSet
+
+	// Global service objects.
+	keysAPI       etcd.KeysAPI
+	gazetteClient *gazette.Client
+	httpClient    *http.Client
 )
 
 type varsData struct {
@@ -56,10 +62,12 @@ type readerData struct {
 
 type memberData struct {
 	masters, replicas int
+	states            map[string]string
+	hasUnreadJournals bool
 	totalLag          int64
 }
 
-type ConsumerData struct {
+type consumerData struct {
 	memberNames  []string
 	journalNames []string
 	members      map[string]memberData
@@ -68,7 +76,7 @@ type ConsumerData struct {
 	journalLag map[string]int64
 }
 
-func (cd ConsumerData) TotalLag() int64 {
+func (cd consumerData) TotalLag() int64 {
 	var res int64 = 0
 	for _, lag := range cd.journalLag {
 		res += lag
@@ -77,42 +85,60 @@ func (cd ConsumerData) TotalLag() int64 {
 }
 
 func main() {
-	endpoints.ParseFromEnvironment()
-	log.SetFormatter(new(log.JSONFormatter))
-	flag.Parse()
-	varz.StartDebugListener()
+	flag.Var(&prefixList, "prefix", "Specify an Etcd prefix to check for consumers.")
+	defer varz.Initialize("gazconsumer").Cleanup()
 
-	printLags()
-	//var cdata = makeConsumerData(flag.Arg(0))
-	//printOutput(cdata)
-	// fmt.Printf("Mike Lag: %s", cdata.journalLag)
+	httpClient = &http.Client{
+		Timeout:   5 * time.Second,
+		Transport: gazette.MakeHttpTransport(),
+	}
 
-	//printOutput(cdata)
-}
+	var err error
+	gazetteClient, err = gazette.NewClient(*endpoints.GazetteEndpoint)
+	if err != nil {
+		log.WithField("err", err).Fatal("failed to init gazette client")
+	}
+	etcdClient, err := etcd.New(etcd.Config{
+		Endpoints: []string{"http://" + *endpoints.EtcdEndpoint}})
+	if err != nil {
+		log.WithField("err", err).Fatal("failed to init etcd client")
+	}
+	keysAPI = etcd.NewKeysAPI(etcdClient)
 
-func printOutput(cdata ConsumerData) {
-	if *short {
-		var consumerLag int64
-		for _, member := range cdata.memberNames {
-			var info = cdata.members[member]
-			if info.totalLag != infinityLag {
-				consumerLag += info.totalLag
-			}
+	// Expand |prefixList| into |consumerList|.
+	var consumerList = expandPrefixList(prefixList)
+
+	checkConsumers(consumerList)
+	if *monitor {
+		for _ = range time.Tick(*monitorInterval) {
+			consumerList = expandPrefixList(prefixList)
+			checkConsumers(consumerList)
 		}
-		fmt.Println(consumerLag / bytesPerGigabyte)
-	} else {
-		printLong(cdata, os.Stdout)
 	}
 }
 
-func printLong(cdata ConsumerData, out io.Writer) {
+func checkConsumers(consumerList []string) {
+	for _, consumer := range consumerList {
+		var cdata = makeConsumerData(consumer)
+		varz.ObtainCount("gazette", "consumer", "lag", "#consumer", consumer).Set(cdata.TotalLag())
+		if !*monitor {
+			printLong(cdata, os.Stdout)
+		}
+	}
+}
+
+func printLong(cdata consumerData, out io.Writer) {
 	var journalsTable = tablewriter.NewWriter(out)
 	journalsTable.SetHeader([]string{"Journal", "Lag", "Owner"})
 	sort.Strings(cdata.journalNames)
 	for _, journal := range cdata.journalNames {
 		var owner = cdata.owners[journal]
-		if lag, ok := cdata.journalLag[journal]; !ok || lag == journalNotBeingRead {
+		if lag, ok := cdata.journalLag[journal]; !ok {
 			journalsTable.Append([]string{journal, "<no data>", owner})
+		} else if cdata.members[owner].states[journal] == "recovering" {
+			journalsTable.Append([]string{journal, "<recovering>", owner})
+		} else if lag == journalNotBeingRead {
+			journalsTable.Append([]string{journal, "<owned, not reading>", owner})
 		} else if lag == 0 {
 			journalsTable.Append([]string{journal, "<up-to-date>", owner})
 		} else {
@@ -125,50 +151,31 @@ func printLong(cdata ConsumerData, out io.Writer) {
 	sort.Strings(cdata.memberNames)
 	for _, member := range cdata.memberNames {
 		var info = cdata.members[member]
-		var lagStr string
-		if info.totalLag == infinityLag {
-			lagStr = "\u221e"
-		} else {
-			lagStr = humanize.Bytes(uint64(info.totalLag))
+		var lagFmt = "%s"
+		if info.hasUnreadJournals {
+			lagFmt = ">= %s (incomplete data)"
 		}
 
 		membersTable.Append([]string{
-			member, strconv.Itoa(info.masters), strconv.Itoa(info.replicas), lagStr})
+			member, strconv.Itoa(info.masters), strconv.Itoa(info.replicas),
+			fmt.Sprintf(lagFmt, humanize.Bytes(uint64(info.totalLag)))})
 	}
 	membersTable.Render()
 }
 
-func makeConsumerData(topic string) ConsumerData {
-	var cdata ConsumerData
-	cdata.members = make(map[string]memberData)
-	cdata.owners = make(map[string]string)
-	readHeads, writeHeads := getHeads(topic, &cdata)
-	getJournalLag(writeHeads, readHeads, &cdata)
-	return cdata
-}
-
-func makeServices(topic string) (*http.Client, *gazette.Client, *etcd.Response) {
-	var httpClient = &http.Client{
-		Timeout:   5 * time.Second,
-		Transport: gazette.MakeHttpTransport(),
-	}
-	var gazetteClient, err = gazette.NewClient(*endpoints.GazetteEndpoint)
-	if err != nil {
-		log.WithField("err", err).Fatal("failed to init gazette client")
-	}
-	etcdClient, err := etcd.New(etcd.Config{
-		Endpoints: []string{"http://" + *endpoints.EtcdEndpoint}})
-	if err != nil {
-		log.WithField("err", err).Fatal("failed to init etcd client")
-	}
-	var keysAPI = etcd.NewKeysAPI(etcdClient)
-	var key = path.Join(topic, "items")
+func makeConsumerData(consumerPath string) consumerData {
+	var key = path.Join(consumerPath, "items")
 	shardsRoot, err := keysAPI.Get(context.Background(), key,
 		&etcd.GetOptions{Recursive: true, Sort: true})
 	if err != nil {
 		log.WithFields(log.Fields{"err": err, "key": key}).Fatal("can't load etcd key")
 	}
-	return httpClient, gazetteClient, shardsRoot
+	var cdata consumerData
+	cdata.members = make(map[string]memberData)
+	cdata.owners = make(map[string]string)
+	readHeads, writeHeads := getHeads(shardsRoot, &cdata)
+	getJournalLag(writeHeads, readHeads, &cdata)
+	return cdata
 }
 
 type MemberInfo struct {
@@ -227,9 +234,7 @@ func getMasterRepInfo(topic string) []MemberInfo {
 	return memberInfos
 }
 
-func getHeads(topic string, cdata *ConsumerData) (map[string]int64, map[string]int64) {
-	httpClient, gazetteClient, shardsRoot := makeServices(topic)
-
+func getHeads(shardsRoot *etcd.Response, cdata *consumerData) (map[string]int64, map[string]int64) {
 	var readHeadOutput = make(chan journalHeadResult, 1024)
 	var writeHeadOutput = make(chan journalHeadResult, 1024)
 	var readHeadWg = new(sync.WaitGroup)
@@ -259,7 +264,11 @@ func getHeads(topic string, cdata *ConsumerData) (map[string]int64, map[string]i
 
 			if !ok {
 				cdata.memberNames = append(cdata.memberNames, memberID)
+				info.states = make(map[string]string)
 			}
+
+			// Stores "ready" or "recovering" state.
+			info.states[journal] = member.Value
 
 			if i == 0 {
 				info.masters++
@@ -309,7 +318,7 @@ func collectHeads(readHeadWg *sync.WaitGroup, readHeadOutput chan journalHeadRes
 	return readHeads, writeHeads
 }
 
-func getJournalLag(writeHeads map[string]int64, readHeads map[string]int64, cdata *ConsumerData) map[string]int64 {
+func getJournalLag(writeHeads map[string]int64, readHeads map[string]int64, cdata *consumerData) map[string]int64 {
 	// Determine total lag value per-member.
 	var journalLag = make(map[string]int64)
 	for journal, writeHead := range writeHeads {
@@ -319,8 +328,8 @@ func getJournalLag(writeHeads map[string]int64, readHeads map[string]int64, cdat
 			// |journalLag[journal]| remains unavailable.
 		} else if readHead == journalNotBeingRead {
 			journalLag[journal] = journalNotBeingRead
-			member.totalLag = infinityLag
-		} else if member.totalLag != infinityLag {
+			member.hasUnreadJournals = true
+		} else {
 			var delta = writeHead - readHead
 			if delta >= minimumLag {
 				journalLag[journal] = delta
@@ -374,4 +383,45 @@ func fetchReadHead(name string, client *http.Client, url string, output chan<- j
 	}
 	output <- result
 	wg.Done()
+}
+
+func expandPrefixList(prefixList []string) []string {
+	var consumerList []string
+	for i := 0; i < len(prefixList); i++ {
+		var prefix = prefixList[i]
+		var prefixNode, err = keysAPI.Get(context.Background(), prefix, nil)
+		if err != nil {
+			log.WithFields(log.Fields{"err": err, "key": prefix}).Fatal("could not get Etcd key")
+		}
+
+		var isConsumer bool
+		for _, child := range prefixNode.Node.Nodes {
+			if base := path.Base(child.Key); base == "items" || base == "members" {
+				// Path is a consumer root.
+				consumerList = append(consumerList, prefix)
+				isConsumer = true
+				break
+			}
+		}
+		if !isConsumer {
+			for _, child := range prefixNode.Node.Nodes {
+				// Path is a directory that contains consumer roots. Check each
+				// one individually.
+				prefixList = append(prefixList, child.Key)
+			}
+		}
+	}
+
+	return consumerList
+}
+
+type prefixFlagSet []string
+
+func (f *prefixFlagSet) String() string {
+	return strings.Join(*f, ",")
+}
+
+func (f *prefixFlagSet) Set(prefix string) error {
+	*f = append(*f, prefix)
+	return nil
 }
