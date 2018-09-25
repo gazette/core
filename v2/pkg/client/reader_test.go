8c7d567aef5c24b02ab80ce415dd4fdf79282280
Johnny Graettinger <johnny531@gmail.com>
2018-09-24 23:04:25 -0400

    client: protocol dispatcher, AsyncAppend dependencies, & PolledList
    Remove Dialer and RoutingClient. Add RouteCache, which deals only
    with concerns of Route caching and is a protocol.DispatchRouter. Update
    and clarify interfaces for RoutedJournalClient, AsyncJournalClient.
    
    Update AppendService to track priors of an Append being started.  These
    are AsyncAppend operations which must complete before the current append
    operation can be started. This mechanism can be used to build comprehensive
    cross-journal write barriers.
    
    Add ListAll and PolledList for periodic enumeration of JournalSpecs
    matching a selector.

--- v2/pkg/client/reader_test.go
+++ v2/pkg/client/reader_test.go
@@ -9,12 +9,12 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/LiveRamp/gazette/v2/pkg/broker/teststub"
+	"github.com/LiveRamp/gazette/v2/pkg/codecs"
+	pb "github.com/LiveRamp/gazette/v2/pkg/protocol"
 	gc "github.com/go-check/check"
-
-	"github.com/LiveRamp/gazette/pkg/broker/teststub"
-	"github.com/LiveRamp/gazette/pkg/codecs"
-	pb "github.com/LiveRamp/gazette/pkg/protocol"
 )
 
 type ReaderSuite struct{}
@@ -33,6 +33,7 @@ func (s *ReaderSuite) TestOpenFragmentURLCases(c *gc.C) {
 	b, err := ioutil.ReadAll(rc)
 	c.Check(err, gc.IsNil)
 	c.Check(string(b), gc.Equals, "XXXXXhello, world!!!")
+	c.Check(rc.Close(), gc.IsNil)
 
 	// Case: read a portion of the fragment.
 	rc, err = OpenFragmentURL(ctx, frag, frag.Begin+5, url)
@@ -41,6 +42,7 @@ func (s *ReaderSuite) TestOpenFragmentURLCases(c *gc.C) {
 	b, err = ioutil.ReadAll(rc)
 	c.Check(err, gc.IsNil)
 	c.Check(string(b), gc.Equals, "hello, world!!!")
+	c.Check(rc.Close(), gc.IsNil)
 
 	// Case: stream ends before Fragment.End.
 	frag.End += 1
@@ -50,6 +52,7 @@ func (s *ReaderSuite) TestOpenFragmentURLCases(c *gc.C) {
 	b, err = ioutil.ReadAll(rc)
 	c.Check(err, gc.Equals, io.ErrUnexpectedEOF)
 	c.Check(string(b), gc.Equals, "hello, world!!!")
+	c.Check(rc.Close(), gc.IsNil)
 
 	// Case: stream continues after Fragment.End.
 	frag.End -= 4
@@ -59,6 +62,11 @@ func (s *ReaderSuite) TestOpenFragmentURLCases(c *gc.C) {
 	b, err = ioutil.ReadAll(rc)
 	c.Check(err, gc.Equals, ErrDidNotReadExpectedEOF)
 	c.Check(string(b), gc.Equals, "hello, world")
+	c.Check(rc.Close(), gc.IsNil)
+
+	// Case: doesn't exist.
+	rc, err = OpenFragmentURL(ctx, frag, frag.Begin, url+"does-not-exist")
+	c.Check(err, gc.ErrorMatches, `!OK fetching \(404 Not Found, "file:///.*\)`)
 
 	// Case: decompression fails.
 	frag.CompressionCodec = pb.CompressionCodec_SNAPPY
@@ -75,6 +83,7 @@ func (s *ReaderSuite) TestReaderCases(c *gc.C) {
 	defer cancel()
 
 	var broker = teststub.NewBroker(c, ctx)
+	var rjc = pb.NewRoutedJournalClient(broker.MustClient(), NewRouteCache(2, time.Hour))
 
 	go serveReadFixtures(c, broker,
 		// Case 1: fixture returns fragment metadata & URL, then EOF.
@@ -100,15 +109,22 @@ func (s *ReaderSuite) TestReaderCases(c *gc.C) {
 	)
 
 	// Case 1: fragment metadata & URL.
-	var r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	var r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 	var b, err = ioutil.ReadAll(r)
 
 	// Expect fragment URL fixture is opened & read directly.
 	c.Check(string(b), gc.Equals, "hello, world!!!")
 	c.Check(err, gc.Equals, nil)
 
+	// And that the Route is cached.
+	c.Check(rjc.Route(ctx, "a/journal"), gc.DeepEquals, pb.Route{
+		Members:   []pb.ProcessSpec_ID{{Zone: "a", Suffix: "broker"}},
+		Endpoints: []pb.Endpoint{broker.Endpoint()},
+		Primary:   0,
+	})
+
 	// Case 2: fragment metadata, no URL and the offset is jumped.
-	r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: -1})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: -1})
 	n, err := r.Read(b)
 
 	c.Check(n, gc.Equals, 0)
@@ -121,49 +137,41 @@ func (s *ReaderSuite) TestReaderCases(c *gc.C) {
 	c.Check(err, gc.Equals, io.EOF)
 
 	// Case 3: NOT_JOURNAL_BROKER => ErrNotJournalBroker.
-	r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 	b, err = ioutil.ReadAll(r)
 
 	c.Check(b, gc.HasLen, 0)
 	c.Check(err, gc.Equals, ErrNotJournalBroker)
 
 	// Case 4: read content, then OFFSET_NOT_YET_AVAILABLE => ErrOffsetNotYetAvailable.
-	r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 	b, err = ioutil.ReadAll(r)
 
 	c.Check(string(b), gc.Equals, "prior content")
 	c.Check(err, gc.Equals, ErrOffsetNotYetAvailable)
 
 	// Case 5: immediate OFFSET_NOT_YET_AVAILABLE => ErrOffsetNotYetAvailable.
-	r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 	b, err = ioutil.ReadAll(r)
 
 	c.Check(b, gc.HasLen, 0)
 	c.Check(err, gc.Equals, ErrOffsetNotYetAvailable)
 
 	// Case 6: expect status is dynamically mapped to error.
-	r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 	b, err = ioutil.ReadAll(r)
 
 	c.Check(b, gc.HasLen, 0)
 	c.Check(err, gc.ErrorMatches, pb.Status_INSUFFICIENT_JOURNAL_BROKERS.String())
 
 	// Case 7: streamed content with multiple small reads, offset jump & tracked Route update
-	var routes = make(map[pb.Journal]*pb.Route)
-	r = NewReader(ctx, routeWrapper{broker.MustClient(), routes},
-		pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 
 	_, err = r.Read(nil)
 	c.Check(err, gc.Equals, ErrOffsetJump)
 
 	// Expect offset was skipped forward to Response.Offset.
 	c.Check(r.Request.Offset, gc.Equals, int64(110))
-
-	// Expect Reader advised of the updated Route.
-	c.Check(routes["a/journal"], gc.DeepEquals, &pb.Route{
-		Members: []pb.ProcessSpec_ID{{Zone: "a", Suffix: "broker"}},
-		Primary: 0,
-	})
 
 	// Note the fixture content is split across two ReadResponses.
 	// Consume both messages across four small Reads.
@@ -199,44 +207,25 @@ func (s *ReaderSuite) TestReaderCases(c *gc.C) {
 	c.Check(r.Request.Offset, gc.Equals, int64(126))
 
 	// Case 8: content followed by a non-EOF error.
-	r = NewReader(ctx, routeWrapper{broker.MustClient(), routes},
-		pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 
 	b, err = ioutil.ReadAll(r)
 	c.Check(string(b), gc.Equals, "partial read")
 	c.Check(err, gc.ErrorMatches, `rpc error: code = Unknown desc = potato`)
 
-	// Expect Reader purged the Route advisement due to non-EOF close.
-	c.Check(routes["a/journal"], gc.IsNil)
-
 	// Case 9: invalid offset jump 105 => 100.
-	delete(routes, "a/journal")
-
-	r = NewReader(ctx, routeWrapper{broker.MustClient(), routes},
-		pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 
 	b, err = ioutil.ReadAll(r)
 	c.Check(b, gc.HasLen, 0)
 	c.Check(err, gc.ErrorMatches, `invalid ReadResponse offset \(100; expected >= 105\)`)
 
-	// Expect Reader purged the Route advisement due to non-EOF close.
-	c.Check(routes, gc.HasLen, 1) // Nil Route was re-added.
-	c.Check(routes["a/journal"], gc.IsNil)
-
 	// Case 10: ReadResponse validation fails (invalid fragment).
-	delete(routes, "a/journal")
-
-	r = NewReader(ctx, routeWrapper{broker.MustClient(), routes},
-		pb.ReadRequest{Journal: "a/journal", Offset: 105})
+	r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 105})
 
 	b, err = ioutil.ReadAll(r)
 	c.Check(b, gc.HasLen, 0)
 	c.Check(err, gc.ErrorMatches, `ReadResponse.Fragment.Journal: invalid length .*`)
-
-	// Expect Reader purged the Route advisement due to non-EOF close.
-	c.Check(routes, gc.HasLen, 1) // Nil Route was re-added.
-	c.Check(routes["a/journal"], gc.IsNil)
-
 }
 
 func (s *ReaderSuite) TestBufferedOffsetAdjustment(c *gc.C) {
@@ -244,9 +233,10 @@ func (s *ReaderSuite) TestBufferedOffsetAdjustment(c *gc.C) {
 	defer cancel()
 
 	var broker = teststub.NewBroker(c, ctx)
+	var rjc = pb.NewRoutedJournalClient(broker.MustClient(), pb.NoopDispatchRouter{})
 	go readFixture{content: "foobar\nbaz\n", offset: 100}.serve(c, broker)
 
-	var r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal", Offset: 100})
+	var r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal", Offset: 100})
 	var br = bufio.NewReader(r)
 
 	var b, err = br.ReadBytes('\n')
@@ -268,12 +258,13 @@ func (s *ReaderSuite) TestReaderSeekCases(c *gc.C) {
 	defer cancel()
 
 	var broker = teststub.NewBroker(c, ctx)
+	var rjc = pb.NewRoutedJournalClient(broker.MustClient(), pb.NoopDispatchRouter{})
 
 	// Start read fixture which returns fragment metadata & URL, then EOF,
 	// causing Reader to directly open the fragment.
 	go readFixture{fragment: &frag, fragmentUrl: url, offset: frag.Begin}.serve(c, broker)
 
-	var r = NewReader(ctx, broker.MustClient(), pb.ReadRequest{Journal: "a/journal"})
+	var r = NewReader(ctx, rjc, pb.ReadRequest{Journal: "a/journal"})
 
 	// First zero-byte read causes Reader to read the response fixture.
 	var n, err = r.Read(nil)
@@ -309,14 +300,6 @@ func (s *ReaderSuite) TestReaderSeekCases(c *gc.C) {
 	c.Check(err, gc.Equals, ErrSeekRequiresNewReader)
 }
 
-// RouteWrapper implements the RouteUpdater interface, for inspections by tests.
-type routeWrapper struct {
-	pb.BrokerClient
-	routes map[pb.Journal]*pb.Route
-}
-
-func (w routeWrapper) UpdateRoute(journal pb.Journal, route *pb.Route) { w.routes[journal] = route }
-
 type readFixture struct {
 	status pb.Status
 	err    error
@@ -335,7 +318,7 @@ func (f readFixture) serve(c *gc.C, broker *teststub.Broker) {
 	// Start with a basic response template which may be customized
 	var resp = &pb.ReadResponse{
 		Status:    pb.Status_OK,
-		Header:    headerFixture,
+		Header:    buildHeaderFixture(broker),
 		Offset:    req.Offset,
 		WriteHead: 1024,
 		Fragment: &pb.Fragment{
@@ -413,14 +396,13 @@ func buildFragmentFixture(c *gc.C) (frag pb.Fragment, url string, dir string, cl
 	return
 }
 
-var (
-	_ = gc.Suite(&ReaderSuite{})
-
-	headerFixture = &pb.Header{
+func buildHeaderFixture(ep interface{ Endpoint() pb.Endpoint }) *pb.Header {
+	return &pb.Header{
 		ProcessId: pb.ProcessSpec_ID{Zone: "a", Suffix: "broker"},
 		Route: pb.Route{
-			Members: []pb.ProcessSpec_ID{{Zone: "a", Suffix: "broker"}},
-			Primary: 0,
+			Members:   []pb.ProcessSpec_ID{{Zone: "a", Suffix: "broker"}},
+			Endpoints: []pb.Endpoint{ep.Endpoint()},
+			Primary:   0,
 		},
 		Etcd: pb.Header_Etcd{
 			ClusterId: 12,
@@ -429,6 +411,8 @@ var (
 			RaftTerm:  78,
 		},
 	}
-)
+}
+
+var _ = gc.Suite(&ReaderSuite{})
 
 func Test(t *testing.T) { gc.TestingT(t) }
