9be0e1091eec868b0066862ec4699802072413ee
Johnny Graettinger <johnny531@gmail.com>
2018-09-24 23:27:53 -0400

    codecs: de/compression support for CompressionCodecs
    Package codecs implements de/compression support for
    protocol.CompressionCodec values.
    
    Issue #72

---
+++ v2/pkg/codecs/zstandard_enable.go
@@ -0,0 +1,14 @@
+// +build !nozstd
+
+package codecs
+
+import (
+	"io"
+
+	"github.com/DataDog/zstd"
+)
+
+func init() {
+	zstdNewReader = func(r io.Reader) (io.ReadCloser, error) { return zstd.NewReader(r), nil }
+	zstdNewWriter = func(w io.Writer) (io.WriteCloser, error) { return zstd.NewWriter(w), nil }
+}
