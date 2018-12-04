4d0754133c3b7b2c2ee2c4ad409d5a47c1bc3845
Rupert Chen <rupert.chen@liveramp.com>
2018-12-04 16:38:34 -0500

    Leverage cached image layers in CircleCI (#111)
    The image liveramp/gazette-base:2.1.0 is an up-to-date build of the
    "base" stage. Most importantly, it allows subsequent builds to skip
    building RocksDB.
    
    Note, since we're using --cache-from to get the base image, the local
    build cache will not be used. As such, we must continue using
    --cache-from on all downstream builds and not rely on the local build
    cache at all.

--- v2/build/all.sh
+++ v2/build/all.sh
@@ -4,6 +4,8 @@ set -Eeu -o pipefail
 # Root directory of repository.
 ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 
+docker pull liveramp/gazette-base:2.1.0
+
 # Build and test Gazette. This image includes all Gazette source, vendored
 # dependencies, compiled packages and binaries, and only completes after
 # all tests pass.
@@ -11,18 +13,21 @@ docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target build \
     --tag liveramp/gazette-build:latest \
-    --cache-from liveramp/gazette-build:latest
+    --cache-from liveramp/gazette-base:2.1.0
 
 # Create the `gazette` image, which plucks the `gazette`, `gazctl` and
 # `run-consumer` onto a base runtime image.
 docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target gazette \
-    --tag liveramp/gazette:latest
+    --tag liveramp/gazette:latest \
+    --cache-from liveramp/gazette-build:latest
 
 # Create the `gazette-examples` image, which further plucks `stream-sum` and
 # `word-count` example artifacts onto the `gazette` image.
 docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target examples \
-    --tag liveramp/gazette-examples:latest
+    --tag liveramp/gazette-examples:latest \
+    --cache-from liveramp/gazette-build:latest \
+    --cache-from liveramp/gazette:latest
