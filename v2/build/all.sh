ffef822479dbedcb04ffe995be04701823346c3d
Rupert Chen <rupert.chen@liveramp.com>
2018-12-10 13:55:12 -0500

    Introduce "vendor" image as cache
    The vendor image replaces the "base" image as an image cache during CI
    builds. It contains everything that was in the "base" target and
    additionally adds the vendored source consistent with the latest
    Gopkg.lock on the master branch.
    
    Use of "docker --cache-from" is removed from non-CI builds (e.g., local
    developer builds) as docker's local build cache persists between
    executions and better handles cached image invalidation.

--- v2/build/all.sh
+++ v2/build/all.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
+set -Eeux -o pipefail
 
 # Root directory of repository.
-ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+readonly ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 
-docker pull liveramp/gazette-base:2.1.0
+readonly VENDOR_IMAGE="rupertchen/gazette-vendor:latest"
+if [[ ${CIRCLECI:-} = true ]]; then
+    docker pull "$VENDOR_IMAGE"
+
+    readonly CF_BUILD="--cache-from $VENDOR_IMAGE"
+    readonly CF_GAZETTE="--cache-from liveramp/gazette-build:latest"
+    readonly CF_EXAMPLES="--cache-from liveramp/gazette-build:latest --cache-from liveramp/gazette:latest"
+
+    if [[ $CIRCLE_BRANCH = "master" ]]; then
+        SHOULD_PUSH_VENDOR=true
+    fi
+fi
 
 # Build and test Gazette. This image includes all Gazette source, vendored
 # dependencies, compiled packages and binaries, and only completes after
@@ -13,7 +24,7 @@ docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target build \
     --tag liveramp/gazette-build:latest \
-    --cache-from liveramp/gazette-base:2.1.0
+    ${CF_BUILD:-}
 
 # Create the `gazette` image, which plucks the `gazette`, `gazctl` and
 # `run-consumer` onto a base runtime image.
@@ -21,7 +32,7 @@ docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target gazette \
     --tag liveramp/gazette:latest \
-    --cache-from liveramp/gazette-build:latest
+    ${CF_GAZETTE:-}
 
 # Create the `gazette-examples` image, which further plucks `stream-sum` and
 # `word-count` example artifacts onto the `gazette` image.
@@ -29,5 +40,20 @@ docker build ${ROOT} \
     --file ${ROOT}/v2/build/Dockerfile \
     --target examples \
     --tag liveramp/gazette-examples:latest \
-    --cache-from liveramp/gazette-build:latest \
-    --cache-from liveramp/gazette:latest
+    ${CF_EXAMPLES:-}
+
+# Publish vendor image which is used as a cache in CI builds.
+if [[ ${SHOULD_PUSH_VENDOR:-} = true && -n "${DOCKER_USER:-}" && -n "${DOCKER_PASS:-}" ]]; then
+    # Temporarily disable xtrace to hide password ($DOCKER_PASS).
+    set +x
+    echo "Log in to Docker as $DOCKER_USER"
+    echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
+    set -x
+
+    docker build . \
+      --file ./v2/build/Dockerfile \
+      --target vendor \
+      --tag "$VENDOR_IMAGE" \
+      --cache-from "$VENDOR_IMAGE"
+    docker push "$VENDOR_IMAGE"
+fi
