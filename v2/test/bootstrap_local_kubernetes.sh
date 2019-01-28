211ff4aa4866e9cf8642a22b7eb3dba43381a313
Johnny Graettinger <johnny.graettinger@liveramp.com>
2019-01-28 16:17:28 -0500

    test: refactor to break up deploy_test_environment.sh
    Add lib.sh, deploy_brokers.sh, and deploy_examples.sh
    
    lib.sh has a number of routines which are used by Gazette and, potentially,
    client applications. deploy_brokers.sh bootstraps an empty broker cluster
    into a given context & namespace. deploy_examples.sh runs examples into a
    given context & namespace, which may differ from the broker namespace.
    
    Update README.rst's.

--- v2/test/bootstrap_local_kubernetes.sh
+++ v2/test/bootstrap_local_kubernetes.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+readonly V2DIR="$(CDPATH= cd "$(dirname "$0")/.." && pwd)"
 readonly USAGE="Usage: $0 kube-context"
-readonly CONTEXT="${1?Kubernetes context is required. $USAGE}"
 
-# KUBECTL defaults to `kubectl`, and uses an explicit --context.
-readonly KUBECTL="${KUBECTL:-kubectl} --context ${CONTEXT}"
-
-# HELM defaults to `helm`, and uses an explicit --context and --kubeconfig.
-# KUBECTL may use a non-standard location for its config (eg, microk8s.kubectl
-# manages kubernetes config under its /snap directory). We make configuration
-# explicit by copying into a tempfile and passing to helm by flag.
-readonly TMPKUBECONFIG=$(mktemp)
-readonly HELM="${HELM:-helm} --kube-context ${CONTEXT} --kubeconfig ${TMPKUBECONFIG}"
-trap "{ rm -f ${TMPKUBECONFIG}; }" EXIT
-${KUBECTL} config view --raw > ${TMPKUBECONFIG}
+. "${V2DIR}/test/lib.sh"
+configure_environment "${1?Kubernetes context is required. $USAGE}"
 
 # Create a service account for helm's server component "tiller" to use, with the cluster-admin role.
 ${KUBECTL} create serviceaccount -n kube-system tiller || true
 ${KUBECTL} create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller || true
 
 # Install tiller into the Kubernetes cluster, and wait for its pod to startup.
-${HELM} init --service-account tiller --wait
+${HELM} init --service-account tiller --upgrade --wait
 
 # Add the "incubator" repository, which is relied on for the "incubator/etcd" chart.
 ${HELM} repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
