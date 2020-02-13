968e7c55701dc6d961b44a8deb59b3f45aa9574b
Michael Schiff <schiff.michael@gmail.com>
2020-02-12 16:48:08 -0800

    first attempt at JsonFrameable interface allowing users to specify how they want to serialize JSON instead of always using encoding/json

--- message/json_framing.go
+++ message/json_framing.go
@@ -3,17 +3,31 @@ package message
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 
 	"go.gazette.dev/core/labels"
 )
 
 type jsonFraming struct{}
 
+type JsonFrameable interface {
+	MarshalJsonTo(*bufio.Writer) (int, error)
+	UnmarshalJson([]byte) error
+}
+
 // ContentType returns labels.ContentType_JSONLines.
 func (*jsonFraming) ContentType() string { return labels.ContentType_JSONLines }
 
 // Marshal implements Framing.
 func (*jsonFraming) Marshal(msg Frameable, bw *bufio.Writer) error {
+	var _, ok = msg.(JsonFrameable)
+	if !ok {
+		return fmt.Errorf("%#v is not a JsonFrameable", msg)
+	}
+	if jf, ok := msg.(JsonFrameable); ok {
+		_, err := jf.MarshalJsonTo(bw)
+		return err
+	}
 	return json.NewEncoder(bw).Encode(msg)
 }
 
@@ -21,12 +35,16 @@ func (*jsonFraming) Marshal(msg Frameable, bw *bufio.Writer) error {
 func (*jsonFraming) NewUnmarshalFunc(r *bufio.Reader) UnmarshalFunc {
 	// We cannot use json.NewDecoder, as it buffers internally beyond the
 	// precise boundary of a JSON message.
-	return func(f Frameable) error {
-		if l, err := UnpackLine(r); err != nil {
-			return err
-		} else {
-			return json.Unmarshal(l, f)
+	return func(f Frameable) (err error) {
+		var l []byte
+		if l, err = UnpackLine(r); err != nil {
+			return
 		}
+		if jf, ok := f.(JsonFrameable); ok {
+			return jf.UnmarshalJson(l)
+		}
+		return json.Unmarshal(l, f)
+
 	}
 }
 
