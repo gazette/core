681fe2210193b67ae5049b117bca518817749a13
Johnny Graettinger <johnny531@gmail.com>
2020-05-29 14:30:50 -0500

    metrics: remove metric key indirection
    This is intended to make it easier to refactor metrics into packages.

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,91 +2,72 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
-// Key constants are exported primarily for documentation reasons. Typically,
-// they will not be used programmatically outside of defining the collectors.
-
 // Keys for gazette metrics.
 const (
-	AllocatorAssignmentAddedTotalKey    = "gazette_allocator_assignment_added_total"
-	AllocatorAssignmentPackedTotalKey   = "gazette_allocator_assignment_packed_total"
-	AllocatorAssignmentRemovedTotalKey  = "gazette_allocator_assignment_removed_total"
-	AllocatorConvergeTotalKey           = "gazette_allocator_converge_total"
-	AllocatorMaxFlowRuntimeSecondsKey   = "gazette_allocator_max_flow_runtime_seconds"
-	AllocatorNumItemSlotsKey            = "gazette_allocator_desired_replication_slots"
-	AllocatorNumItemsKey                = "gazette_allocator_items"
-	AllocatorNumMembersKey              = "gazette_allocator_members"
-	CommitsTotalKey                     = "gazette_commits_total"
-	CommittedBytesTotalKey              = "gazette_committed_bytes_total"
-	JournalServerResponseTimeSecondsKey = "gazette_journal_server_response_time_seconds"
-	RecoveryLogRecoveredBytesTotalKey   = "gazette_recoverylog_recovered_bytes_total"
-	StorePersistedBytesTotalKey         = "gazette_store_persisted_bytes_total"
-	StoreRequestsTotalKey               = "gazette_store_requests_total"
-	WriteHeadKey                        = "gazette_write_head"
-
 	Fail = "fail"
 	Ok   = "ok"
 )
 
-// Collectors for gazette metrics.
+// Collectors for Gazette broker & consumer metrics.
 var (
 	AllocatorAssignmentAddedTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: AllocatorAssignmentAddedTotalKey,
+		Name: "gazette_allocator_assignment_added_total",
 		Help: "Cumulative number of item / member assignments added by the allocator.",
 	})
 	AllocatorAssignmentPackedTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: AllocatorAssignmentPackedTotalKey,
+		Name: "gazette_allocator_assignment_packed_total",
 		Help: "Cumulative number of item / member assignments packed by the allocator.",
 	})
 	AllocatorAssignmentRemovedTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: AllocatorAssignmentRemovedTotalKey,
+		Name: "gazette_allocator_assignment_removed_total",
 		Help: "Cumulative number of item / member assignments removed by the allocator.",
 	})
 	AllocatorConvergeTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: AllocatorConvergeTotalKey,
+		Name: "gazette_allocator_converge_total",
 		Help: "Cumulative number of converge iterations.",
 	})
 	AllocatorMaxFlowRuntimeSeconds = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name: AllocatorMaxFlowRuntimeSecondsKey,
+		Name: "gazette_allocator_max_flow_runtime_seconds",
 		Help: "Duration required to re-solve for maximum assignment.",
 	})
 	AllocatorNumItemSlots = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: AllocatorNumItemSlotsKey,
+		Name: "gazette_allocator_desired_replication_slots",
 		Help: "Number of desired item replication slots summed across all items.",
 	})
 	AllocatorNumItems = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: AllocatorNumItemsKey,
+		Name: "gazette_allocator_items",
 		Help: "Number of items known to the allocator.",
 	})
 	AllocatorNumMembers = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: AllocatorNumMembersKey,
+		Name: "gazette_allocator_members",
 		Help: "Number of members known to the allocator.",
 	})
 	CommittedBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: CommittedBytesTotalKey,
+		Name: "gazette_committed_bytes_total",
 		Help: "Cumulative number of bytes committed to journals.",
 	})
 	CommitsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: CommitsTotalKey,
+		Name: "gazette_commits_total",
 		Help: "Cumulative number of commits.",
 	}, []string{"status"})
 	RecoveryLogRecoveredBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: RecoveryLogRecoveredBytesTotalKey,
+		Name: "gazette_recoverylog_recovered_bytes_total",
 		Help: "Cumulative number of bytes recovered.",
 	})
 	StoreRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: StoreRequestsTotalKey,
+		Name: "gazette_store_requests_total",
 		Help: "Cumulative number of fragment store operations.",
 	}, []string{"provider", "operation", "status"})
 	StorePersistedBytesTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: StorePersistedBytesTotalKey,
+		Name: "gazette_store_persisted_bytes_total",
 		Help: "Cumulative number of bytes persisted to fragment stores.",
 	}, []string{"provider"})
 	JournalServerResponseTimeSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: JournalServerResponseTimeSecondsKey,
+		Name: "gazette_journal_server_response_time_seconds",
 		Help: "Response time of JournalServer.Append.",
 	}, []string{"operation", "status"})
 	WriteHead = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: WriteHeadKey,
+		Name: "gazette_write_head",
 		Help: "Current write head.",
 	}, []string{"journal"})
 )
@@ -111,88 +92,38 @@ func GazetteBrokerCollectors() []prometheus.Collector {
 	}
 }
 
-// Keys for gazretention metrics.
-const (
-	GazretentionDeletedBytesTotalKey      = "gazretention_deleted_bytes_total"
-	GazretentionDeletedFragmentsTotalKey  = "gazretention_deleted_fragments_total"
-	GazretentionRetainedBytesTotalKey     = "gazretention_retained_bytes_total"
-	GazretentionRetainedFragmentsTotalKey = "gazretention_retained_fragments_total"
-)
-
-// Collectors for gazretention metrics.
-var (
-	GazretentionDeletedBytesTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazretentionDeletedBytesTotalKey,
-		Help: "Cumulative number of bytes deleted.",
-	}, []string{"prefix"})
-	GazretentionDeletedFragmentsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazretentionDeletedFragmentsTotalKey,
-		Help: "Cumulative number of fragments deleted.",
-	}, []string{"prefix"})
-	GazretentionRetainedBytesTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazretentionRetainedBytesTotalKey,
-		Help: "Cumulative number of bytes retained.",
-	}, []string{"prefix"})
-	GazretentionRetainedFragmentsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazretentionRetainedFragmentsTotalKey,
-		Help: "Cumulative number of fragments retained.",
-	}, []string{"prefix"})
-)
-
-func GazretentionCollectors() []prometheus.Collector {
-	return []prometheus.Collector{
-		GazretentionDeletedBytesTotal,
-		GazretentionDeletedFragmentsTotal,
-		GazretentionRetainedBytesTotal,
-		GazretentionRetainedFragmentsTotal,
-	}
-}
-
-// Keys for gazette.Client and gazette.WriteService metrics.
-const (
-	GazetteDiscardBytesTotalKey         = "gazette_discard_bytes_total"
-	GazetteReadBytesTotalKey            = "gazette_read_bytes_total"
-	GazetteSequencerQueued              = "gazette_sequencer_queued"
-	GazetteSequencerReplay              = "gazette_sequencer_replay"
-	GazetteWriteBytesTotalKey           = "gazette_write_bytes_total"
-	GazetteWriteCountTotalKey           = "gazette_write_count_total"
-	GazetteWriteDurationSecondsTotalKey = "gazette_write_duration_seconds_total"
-	GazetteWriteFailureTotalKey         = "gazette_write_failure_total"
-)
-
 // Collectors for gazette.Client and gazette.WriteService metrics.
-// TODO(rupert): Should prefix be GazetteClient-, "gazette_client_-"?
 var (
 	GazetteDiscardBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteDiscardBytesTotalKey,
+		Name: "gazette_discard_bytes_total",
 		Help: "Cumulative number of bytes read and discarded during a fragment seek.",
 	})
 	GazetteReadBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteReadBytesTotalKey,
+		Name: "gazette_read_bytes_total",
 		Help: "Cumulative number of bytes read.",
 	})
 	GazetteSequencerQueuedTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazetteSequencerQueued,
+		Name: "gazette_sequencer_queued",
 		Help: "Cumulative number of read-uncommitted messages which were sequenced.",
 	}, []string{"journal", "flag", "outcome"})
 	GazetteSequencerReplayTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: GazetteSequencerReplay,
+		Name: "gazette_sequencer_replay",
 		Help: "Cumulative number of messages re-read from source journal due to insufficient Sequencer ring-buffer size.",
 	}, []string{"journal"})
 	GazetteWriteBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteWriteBytesTotalKey,
+		Name: "gazette_write_bytes_total",
 		Help: "Cumulative number of bytes written.",
 	})
 	GazetteWriteCountTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteWriteCountTotalKey,
+		Name: "gazette_write_count_total",
 		Help: "Cumulative number of writes.",
 	})
 	GazetteWriteDurationTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteWriteDurationSecondsTotalKey,
+		Name: "gazette_write_duration_seconds_total",
 		Help: "Cumulative number of seconds spent writing.",
 	})
 	GazetteWriteFailureTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteWriteFailureTotalKey,
+		Name: "gazette_write_failure_total",
 		Help: "Cumulative number of write errors returned to clients.",
 	})
 )
@@ -211,55 +142,42 @@ func GazetteClientCollectors() []prometheus.Collector {
 	}
 }
 
-// Keys for consumer.Runner metrics.
-const (
-	GazetteConsumerTxCountTotalKey          = "gazette_consumer_tx_count_total"
-	GazetteConsumerTxMessagesTotalKey       = "gazette_consumer_tx_messages_total"
-	GazetteConsumerTxSecondsTotalKey        = "gazette_consumer_tx_seconds_total"
-	GazetteConsumerTxConsumeSecondsTotalKey = "gazette_consumer_tx_consume_seconds_total"
-	GazetteConsumerTxStalledSecondsTotalKey = "gazette_consumer_tx_stalled_seconds_total"
-	GazetteConsumerTxFlushSecondsTotalKey   = "gazette_consumer_tx_flush_seconds_total"
-	GazetteConsumerTxSyncSecondsTotalKey    = "gazette_consumer_tx_sync_seconds_total"
-	GazetteConsumerConsumedBytesTotalKey    = "gazette_consumer_consumed_bytes_total"
-	GazetteConsumerReadHeadKey              = "gazette_consumer_read_head"
-)
-
 // Collectors for consumer.Runner metrics.
 var (
 	GazetteConsumerTxCountTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxCountTotalKey,
+		Name: "gazette_consumer_tx_count_total",
 		Help: "Cumulative number of transactions",
 	})
 	GazetteConsumerTxMessagesTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxMessagesTotalKey,
+		Name: "gazette_consumer_tx_messages_total",
 		Help: "Cumulative number of messages.",
 	})
 	GazetteConsumerTxSecondsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxSecondsTotalKey,
+		Name: "gazette_consumer_tx_seconds_total",
 		Help: "Cumulative number of seconds processing transactions.",
 	})
 	GazetteConsumerTxConsumeSecondsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxConsumeSecondsTotalKey,
+		Name: "gazette_consumer_tx_consume_seconds_total",
 		Help: "Cumulative number of seconds transactions were processing messages.",
 	})
 	GazetteConsumerTxStalledSecondsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxStalledSecondsTotalKey,
+		Name: "gazette_consumer_tx_stalled_seconds_total",
 		Help: "Cumulative number of seconds transactions were stalled waiting for Gazette IO.",
 	})
 	GazetteConsumerTxFlushSecondsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxFlushSecondsTotalKey,
+		Name: "gazette_consumer_tx_flush_seconds_total",
 		Help: "Cumulative number of seconds transactions were flushing their commit.",
 	})
 	GazetteConsumerTxSyncSecondsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerTxSyncSecondsTotalKey,
+		Name: "gazette_consumer_tx_sync_seconds_total",
 		Help: "Cumulative number of seconds transactions were waiting for their commit to sync.",
 	})
 	GazetteConsumerBytesConsumedTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: GazetteConsumerConsumedBytesTotalKey,
+		Name: "gazette_consumer_consumed_bytes_total",
 		Help: "Cumulative number of bytes consumed.",
 	})
 	GazetteConsumerReadHead = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: GazetteConsumerReadHeadKey,
+		Name: "gazette_consumer_read_head",
 		Help: "Consumer read head",
 	}, []string{"journal"})
 )
