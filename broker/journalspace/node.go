gazette/core

commit 5224810eacb90136d1a88259721b6df6be432f2c

Allow explicit revision of -1 on directory nodes

broker/journalspace/node.go
@@ -67,7 +67,7 @@ func (n Node) IsDir() bool { return len(n.Spec.Name) == 0 || n.Spec.Name[len(n.S
 // checked separately.
 func (n *Node) Validate() error {
 	if n.IsDir() {
-		if n.Revision != 0 {
+		if n.Revision != 0 && n.Revision != -1 {
 			return pb.NewValidationError("unexpected Revision (%d)", n.Revision)
 		} else if len(n.Children) == 0 {
 			return pb.NewValidationError("expected one or more Children")

broker/journalspace/node_test.go
@@ -23,6 +23,9 @@ func (s *NodeSuite) TestValidationCases(c *gc.C) {
 	node.Revision = 0
 	c.Check(node.Validate(), gc.ErrorMatches, `expected one or more Children`)
 
+	node.Revision = -1
+	c.Check(node.Validate(), gc.ErrorMatches, `expected one or more Children`)
+
 	node.Children = append(node.Children,
 		Node{Spec: pb.JournalSpec{Name: "foo"}},
 		Node{Spec: pb.JournalSpec{Name: "bar"}})

commit ef52983b1a6a52587dd8a94aea827efb610637f4

Etcd Revision clarifications (#285)
Updates broker/journalspace/node.go reverting the changes introduced in 5224810eacb90136d1a88259721b6df6be432f2c
and clarifies the documentation of revision:-1

broker/journalspace/node.go
@@ -67,7 +67,7 @@ func (n Node) IsDir() bool { return len(n.Spec.Name) == 0 || n.Spec.Name[len(n.S
 // checked separately.
 func (n *Node) Validate() error {
 	if n.IsDir() {
-		if n.Revision != 0 && n.Revision != -1 {
+		if n.Revision != 0 {
 			return pb.NewValidationError("unexpected Revision (%d)", n.Revision)
 		} else if len(n.Children) == 0 {
 			return pb.NewValidationError("expected one or more Children")

broker/journalspace/node_test.go
@@ -23,9 +23,6 @@ func (s *NodeSuite) TestValidationCases(c *gc.C) {
 	node.Revision = 0
 	c.Check(node.Validate(), gc.ErrorMatches, `expected one or more Children`)
 
-	node.Revision = -1
-	c.Check(node.Validate(), gc.ErrorMatches, `expected one or more Children`)
-
 	node.Children = append(node.Children,
 		Node{Spec: pb.JournalSpec{Name: "foo"}},
 		Node{Spec: pb.JournalSpec{Name: "bar"}})