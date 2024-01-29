fd119b4e69899cf1c3ed1712a18f8b842f993eeb
Johnny Graettinger <johnny@estuary.dev>
2024-01-29 15:52:46 +0000

    WIP - example

--- cmd/gazctl/gazctlcmd/shards_prune_test.go
+++ cmd/gazctl/gazctlcmd/shards_prune_test.go
@@ -1,13 +1,288 @@
 package gazctlcmd
 
 import (
+	"encoding/json"
 	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	pb "go.gazette.dev/core/broker/protocol"
+	pc "go.gazette.dev/core/consumer/protocol"
 	"go.gazette.dev/core/consumer/recoverylog"
 )
+
+func TestFixture(t *testing.T) {
+	var fixture = `{
+    "header": {
+      "process_id": {
+        "zone": "us-central1-c",
+        "suffix": "flow-reactor-74fcd7f9db-n5c5j"
+      },
+      "route": {
+        "members": null,
+        "primary": -1
+      },
+      "etcd": {
+        "cluster_id": 9261053468663690000,
+        "member_id": 14358579373743376000,
+        "revision": 57121751,
+        "raft_term": 140
+      }
+    },
+    "primary_hints": {
+      "hints": {
+        "log": "example/log",
+        "live_nodes": [
+          {
+            "fnode": 1018,
+            "segments": [
+              {
+                "author": 3418428788,
+                "first_seq_no": 1018,
+                "first_offset": 129100,
+                "first_checksum": 2628272215,
+                "last_seq_no": 1024
+              }
+            ]
+          }
+        ],
+        "properties": null
+      }
+    },
+    "backup_hints": [
+      {
+        "hints": {
+          "log": "example/log",
+          "live_nodes": [
+            {
+              "fnode": 1109,
+              "segments": [
+                {
+                  "author": 3418428788,
+                  "first_seq_no": 1109,
+                  "first_offset": 140706,
+                  "first_checksum": 1095500179,
+                  "last_seq_no": 1115,
+                  "last_offset": 141535
+                }
+              ]
+            },
+            {
+              "fnode": 1289,
+              "segments": [
+                {
+                  "author": 2080950356,
+                  "first_seq_no": 1289,
+                  "first_offset": 152255,
+                  "first_checksum": 777889612,
+                  "last_seq_no": 1290,
+                  "last_offset": 153297
+                }
+              ]
+            },
+            {
+              "fnode": 1642,
+              "segments": [
+                {
+                  "author": 245017003,
+                  "first_seq_no": 1642,
+                  "first_offset": 245137,
+                  "first_checksum": 3605342194,
+                  "last_seq_no": 1645,
+                  "last_offset": 251592
+                }
+              ]
+            },
+            {
+              "fnode": 1657,
+              "segments": [
+                {
+                  "author": 3711033845,
+                  "first_seq_no": 1657,
+                  "first_offset": 252114,
+                  "first_checksum": 794580610,
+                  "last_seq_no": 1660,
+                  "last_offset": 252379
+                }
+              ]
+            },
+            {
+              "fnode": 1661,
+              "segments": [
+                {
+                  "author": 3711033845,
+                  "first_seq_no": 1661,
+                  "first_offset": 252379,
+                  "first_checksum": 157414297,
+                  "last_seq_no": 1665,
+                  "last_offset": 252574
+                }
+              ]
+            },
+            {
+              "fnode": 1666,
+              "segments": [
+                {
+                  "author": 3711033845,
+                  "first_seq_no": 1666,
+                  "first_offset": 252574,
+                  "first_checksum": 157986885,
+                  "last_seq_no": 1666,
+                  "last_offset": 252610
+                }
+              ]
+            },
+            {
+              "fnode": 1670,
+              "segments": [
+                {
+                  "author": 3711033845,
+                  "first_seq_no": 1670,
+                  "first_offset": 252737,
+                  "first_checksum": 1747582972,
+                  "last_seq_no": 1673,
+                  "last_offset": 259192
+                }
+              ]
+            }
+          ],
+          "properties": [
+            {
+              "path": "/IDENTITY",
+              "content": "0a4d4620-a93f-468a-9c12-26de543261f0\n"
+            }
+          ]
+        }
+      },
+      {
+        "hints": {
+          "log": "example/log",
+          "live_nodes": [
+            {
+              "fnode": 1109,
+              "segments": [
+                {
+                  "author": 3418428788,
+                  "first_seq_no": 1109,
+                  "first_offset": 140706,
+                  "first_checksum": 1095500179,
+                  "last_seq_no": 1115,
+                  "last_offset": 141535
+                }
+              ]
+            },
+            {
+              "fnode": 1289,
+              "segments": [
+                {
+                  "author": 2080950356,
+                  "first_seq_no": 1289,
+                  "first_offset": 152255,
+                  "first_checksum": 777889612,
+                  "last_seq_no": 1290,
+                  "last_offset": 153297
+                }
+              ]
+            },
+            {
+              "fnode": 1614,
+              "segments": [
+                {
+                  "author": 1701696287,
+                  "first_seq_no": 1614,
+                  "first_offset": 237537,
+                  "first_checksum": 3210869242,
+                  "last_seq_no": 1617,
+                  "last_offset": 243992
+                }
+              ]
+            },
+            {
+              "fnode": 1629,
+              "segments": [
+                {
+                  "author": 245017003,
+                  "first_seq_no": 1629,
+                  "first_offset": 244514,
+                  "first_checksum": 3153286517,
+                  "last_seq_no": 1632,
+                  "last_offset": 244779
+                }
+              ]
+            },
+            {
+              "fnode": 1633,
+              "segments": [
+                {
+                  "author": 245017003,
+                  "first_seq_no": 1633,
+                  "first_offset": 244779,
+                  "first_checksum": 3701750198,
+                  "last_seq_no": 1637,
+                  "last_offset": 244974
+                }
+              ]
+            },
+            {
+              "fnode": 1638,
+              "segments": [
+                {
+                  "author": 245017003,
+                  "first_seq_no": 1638,
+                  "first_offset": 244974,
+                  "first_checksum": 2949463732,
+                  "last_seq_no": 1638,
+                  "last_offset": 245010
+                }
+              ]
+            },
+            {
+              "fnode": 1642,
+              "segments": [
+                {
+                  "author": 245017003,
+                  "first_seq_no": 1642,
+                  "first_offset": 245137,
+                  "first_checksum": 3605342194,
+                  "last_seq_no": 1645,
+                  "last_offset": 251592
+                }
+              ]
+            }
+          ],
+          "properties": [
+            {
+              "path": "/IDENTITY",
+              "content": "0a4d4620-a93f-468a-9c12-26de543261f0\n"
+            }
+          ]
+        }
+      }
+    ]
+  }`
+
+	var resp pc.GetHintsResponse
+	require.NoError(t, json.Unmarshal([]byte(fixture), &resp))
+
+	var set = make(map[pb.Journal]recoverylog.SegmentSet)
+
+	for _, curHints := range append(resp.BackupHints, resp.PrimaryHints) {
+		var hints = curHints.Hints
+
+		// We require that we see _all_ hints for a shards before we may make _any_ deletions.
+		// This is because shards could technically include segments from any log,
+		// and without comprehensive hints which are proof-positive that _no_ shard
+		// references a given journal fragment, we cannot be sure it's safe to remove.
+		// For this reason, we must track the journals to be skipped, so we can be sure
+		// we don't prune journals that are used by a shard that hasn't persisted hints.
+		if hints != nil && len(hints.LiveNodes) > 0 {
+			foldHintsIntoSegments(*hints, set)
+		}
+	}
+
+	require.Equal(t, nil, set)
+
+}
 
 func TestSegmentFoldingWithSingleLog(t *testing.T) {
 	var m = make(map[pb.Journal]recoverylog.SegmentSet)
